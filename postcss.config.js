5041d902e7533c917f9b98a6f414e232befc0cd2
joma74 <dev-mails@gmx.net>
2019-03-21 16:29:49 +0100

    refactor(css): postcss calc for calculating not only css vars but wuth $ postcss vars

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,10 @@ module.exports = {
       variables: { cssenv: process.env.NODE_ENV },
     }),
     // @ts-ignore
+    require("postcss-calc"),
+    // @ts-ignore
+    require("postcss-nesting"),
+    // @ts-ignore
     require("tailwindcss")("./tailwind.js"),
     // @ts-ignore
     require("autoprefixer"),
